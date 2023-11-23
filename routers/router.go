067b58c474e1e951be9c028c5a488a487f36c14f
sunlei <1255193087@qq.com>
2023-11-23 16:35:49 +0800

    重构

--- routers/router.go
+++ routers/router.go
@@ -2,7 +2,6 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
-	"go-web/service/login_service"
 )
 
 func InitRouter() *gin.Engine {
@@ -11,6 +10,5 @@ func InitRouter() *gin.Engine {
 	r.Use(gin.Recovery())
 
 	//r.GET("/auth", api.GetAuth)
-	r.POST("/sys/login", login_service.UserLogin)
 	return r
 }
