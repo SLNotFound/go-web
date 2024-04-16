a3e9b4b240da0e6ca03ba220295cbb4884f20479
Glisseo <haru556549@outlook.com>
2024-04-16 14:18:54 +0800

    初始化

--- main.go
+++ main.go
@@ -1,28 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"github.com/spf13/viper"
-	"go-web/config"
-	"go-web/router"
-	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// 初始化配置
-	if err := config.InitConfiguration(); err != nil {
-		fmt.Printf("init configuration failed, err: %v\n", err)
-		return
-	}
+	r := gin.Default()
 
-	// 初始化路由
-	r := router.Setup(config.Conf.Mode)
-
-	// 启动服务
-	srv := &http.Server{
-		Addr:    fmt.Sprintf(":%d", viper.GetInt("app.port")),
-		Handler: r,
-	}
-
-	srv.ListenAndServe()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello Gin",
+		})
+	})
+	r.Run()
 }
--- service/login/login.go
+++
@@ -1,7 +0,0 @@
-package login
-
-import "github.com/gin-gonic/gin"
-
-func (c *gin.Context) loginHandler() {
-
-}
